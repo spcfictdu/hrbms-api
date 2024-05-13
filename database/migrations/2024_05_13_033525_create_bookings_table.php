94681bbc336a3761bacab5d82fea57e85cb68542
Sypth <105406568+Sypth@users.noreply.github.com>
2024-05-13 11:54:55 +0800

    :wrench: bookings and guests migration created

---
+++ database/migrations/2024_05_13_033525_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id');
+            $table->string('status');
+            $table->enum('payment_type', ['cash', 'gcash']);
+            $table->date('check_in_date');
+            $table->time('check_in_time');
+            $table->date('check_out_date');
+            $table->time('check_out_time');
+            $table->string('number_of_guest');
+            $table->foreignId('guest_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
---
+++ database/migrations/2024_05_13_034352_create_guests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('province');
+            $table->string('city');
+            $table->tinyInteger('phone_number');
+            $table->string('email');
+            $table->string('id_type');
+            $table->string('id_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guests');
+    }
+};
