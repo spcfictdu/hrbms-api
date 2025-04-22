3891220ca4380d91a8cd2b4a90b627b1fd33dd50
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2025-04-22 11:18:06 +0800

    :wrench: Update ReportGenerationController.php
    Apply room occupancy report generation

--- app/Http/Controllers/ReportGeneration/ReportGenerationController.php
+++ app/Http/Controllers/ReportGeneration/ReportGenerationController.php
@@ -17,23 +17,26 @@ use App\Repositories\ReportGeneration\{
     GenerateRevenueReportRepository,
     GeneratePaymentReportRepository,
     GenerateCheckInOutReportRepository,
-    GenerateDailyReservationsReportRepository
+    GenerateDailyReservationsReportRepository,
+    GenerateRoomOccupancyReportRepository
 };
 
 class ReportGenerationController extends Controller
 {
-    protected $revenueReport, $paymentReport, $checkInOutReport, $dailyReservations;
+    protected $revenueReport, $paymentReport, $checkInOutReport, $dailyReservations, $roomOccupancy;
 
     public function __construct(
         GenerateRevenueReportRepository $revenueReport,
         GeneratePaymentReportRepository $paymentReport,
         GenerateCheckInOutReportRepository $checkInOutReport,
-        GenerateDailyReservationsReportRepository $dailyReservations
+        GenerateDailyReservationsReportRepository $dailyReservations,
+        GenerateRoomOccupancyReportRepository $roomOccupancy
     ) {
         $this->revenueReport = $revenueReport;
         $this->paymentReport = $paymentReport;
         $this->checkInOutReport = $checkInOutReport;
         $this->dailyReservations = $dailyReservations;
+        $this->roomOccupancy = $roomOccupancy;
     }
 
     protected function revenueReport(GenerateRevenueReportRequest $request)
@@ -50,9 +53,14 @@ class ReportGenerationController extends Controller
     {
         return $this->checkInOutReport->execute($request);
     }
-    
+
     protected function dailyReservations(Request $request)
     {
         return $this->dailyReservations->execute($request);
+    }
+
+    protected function roomOccupancy(Request $request)
+    {
+        return $this->roomOccupancy->execute($request);
     }
 }
