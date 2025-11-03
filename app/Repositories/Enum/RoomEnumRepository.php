aeb6738bbd2a1ec1386eaada6516b8b1ca5d8432
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2025-11-03 10:17:22 +0800

    🔧 fix bug

--- app/Repositories/Enum/RoomEnumRepository.php
+++ app/Repositories/Enum/RoomEnumRepository.php
@@ -31,7 +31,7 @@ class RoomEnumRepository extends BaseRepository
         $interval = new DateInterval('P1D');
 
         // If same-day checkout or invalid range, include at least one date (the check-in date)
-        if ($begin === $end) {
+        if ($begin == $end) {
             $dates = [$begin->format('Y-m-d')];
         } else {
             $dateRange = new DatePeriod($begin, $interval, $end);
